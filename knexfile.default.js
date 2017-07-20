56501a556d2d4afc081cc85cc5c80b0ed68acf89
nate <nate.clay.williams@gmail.com>
2017-07-20 16:02:06 -0500

    Change default name of databases.

--- knexfile.default.js
+++ knexfile.default.js
@@ -11,7 +11,7 @@ module.exports = {
         client: 'pg',
         connection: {
             host: 'localhost',
-            database: 'practice',
+            database: 'starter',
             user: 'postgres'
         },
         migrations: {
@@ -23,7 +23,7 @@ module.exports = {
         client: 'pg',
         connection: {
             host: 'localhost',
-            database: 'practice_test',
+            database: 'starter_test',
             user: 'postgres'
         },
         migrations: {
